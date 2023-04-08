abbdf9e1b932a09800378c81106fd43ed093a47a
Javierdiaz1386 <javidavi16dd@gmail.com>
2023-04-08 15:06:04 -0500

    Configurando NoneType

--- models/model_task.py
+++ models/model_task.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class TaskUser(BaseModel):
-    id: str | None
+    id: Optional(str)
     username: str
     name: str
     affair: str
